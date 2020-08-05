632e549cd0c34c66af26b4518817fe10c3d54858
jamesylgan <jg893@cornell.edu>
2020-08-04 19:09:54 -0500

    add icons, update education, add hobbies

--- assets/tabs.js
+++ assets/tabs.js
@@ -20,8 +20,10 @@ function openCity(evt, cityName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-document.getElementById("defaultOpenProject").click();
+var tabChooser = Math.floor(Math.random() * 3);
+var tabChooser2 = Math.floor(Math.random() * 3);
+document.getElementById("defaultOpen"+tabChooser).click();
+document.getElementById("defaultOpenProject"+tabChooser2).click();
 
 function openProjects(evt, cityName) {
   // Declare all variables
@@ -42,4 +44,24 @@ function openProjects(evt, cityName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function openHobbies(evt, cityName) {
+  // Declare all variables
+  var i, hobbycontent, hobbylinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  hobbycontent = document.getElementsByClassName("hobbycontent");
+  for (i = 0; i < hobbycontent.length; i++) {
+    if (hobbycontent[i].style.display == "none") {
+      hobbycontent[i].style.display = "block";
+      console.log(evt.currentTarget.className);
+      evt.currentTarget.className += " active";
+      console.log(evt.currentTarget.className);
+    } else {
+      hobbycontent[i].style.display = "none";
+      // remove " active" from the button class name so it doesn't highlight when closed
+      evt.currentTarget.className = evt.currentTarget.className.substring(0, evt.currentTarget.className.length - 7);
+    }
+  }
 }
